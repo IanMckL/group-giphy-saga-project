864f4e68ba8d7d247d8826aa651f55e4c661d888
IanMckL <96411486+IanMckL@users.noreply.github.com>
2022-05-13 11:11:46 -0500

    Added Favorites Router

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useState} from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector} from 'react-redux'
+import { Link, useHistory } from 'react-router-dom';
 import './Search.css'
 
 function Search(){
@@ -8,7 +9,7 @@ function Search(){
 
 //
 const dispatch = useDispatch();
-
+const history = useHistory();
 //REACT useState
 const [search,setSearch]=useState('')
 //FUNCTIONS
@@ -19,19 +20,36 @@ const sendSearch = () =>{
    })
 }
 
+const onSearchClick =() =>{
+    sendSearch();
+    goToHome();
+}
 
 
+const goToHome = () => {
+    history.push('/')
+}
 
+
+const goToFav = () =>{
+    history.push('/favlist')
+}
 return(
 <div className="Search">
-    <h1>Search</h1>
+  
     <div className="searchbar">
-        
+            <h1>Search</h1>
             <input onChange={(e)=>{setSearch(e.target.value)}}className='longinput'></input>
        
  
-            <button onClick={sendSearch} className='searchbutton'>🔎</button>
+            <button onClick={onSearchClick} className='searchbutton'>🔎</button>
+
+            
       
+    </div>
+    <div className='nav-to-fav'>
+        <h1>Favorites</h1>
+        <button onClick={goToFav}className='nav-button'>Favorites</button>
     </div>
 </div>
 )
